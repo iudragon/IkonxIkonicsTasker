6a2b6ff20380dc6dc63d75f7ae69c1cf493198e7
iudragon <42891947+TeamLewok@users.noreply.github.com>
2019-03-16 01:47:56 +0530

    36MealQuantity

--- app/src/main/java/dragon/bakuman/iu/ikonxikonicstasker/Activities/MealDetailActivity.java
+++ app/src/main/java/dragon/bakuman/iu/ikonxikonicstasker/Activities/MealDetailActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -23,7 +25,7 @@ public class MealDetailActivity extends AppCompatActivity {
         String mealId = intent.getStringExtra("mealId");
         String mealName = intent.getStringExtra("mealName");
         String mealDescription = intent.getStringExtra("mealDescription");
-        Float mealPrice = intent.getFloatExtra("mealPrice", 0);
+        final Float mealPrice = intent.getFloatExtra("mealPrice", 0);
         String mealImage = intent.getStringExtra("mealImage");
 
 
@@ -31,13 +33,43 @@ public class MealDetailActivity extends AppCompatActivity {
 
         TextView name = findViewById(R.id.meal_name);
         TextView desc = findViewById(R.id.meal_desc);
-        TextView price = findViewById(R.id.meal_price);
+        final TextView price = findViewById(R.id.meal_price);
         ImageView image = findViewById(R.id.meal_image);
 
         name.setText(mealName);
         desc.setText(mealDescription);
         price.setText("Rs." + mealPrice);
         Picasso.with(getApplicationContext()).load(mealImage).fit().into(image);
+
+        final TextView labelQuantity = findViewById(R.id.label_quantity);
+        Button buttonIncrease = findViewById(R.id.button_increase);
+        Button buttonDecrease = findViewById(R.id.button_decrease);
+
+        buttonIncrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int qty = Integer.parseInt(labelQuantity.getText().toString());
+
+                qty = qty + 1;
+                labelQuantity.setText(qty + "");
+                price.setText("Rs." + (qty * mealPrice));
+
+            }
+        });
+
+
+        buttonDecrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int qty = Integer.parseInt(labelQuantity.getText().toString());
+                if (qty > 1) {
+                    qty = qty - 1;
+                    labelQuantity.setText(qty + "");
+                    price.setText("Rs." + (qty * mealPrice));
+                }
+            }
+        });
+
 
     }
 
