ef046c68a318aeb871040570c366a1b800282e13
iudragon <42891947+TeamLewok@users.noreply.github.com>
2019-03-17 21:45:24 +0530

    52ReadyOrderList

---
+++ app/src/main/java/dragon/bakuman/iu/ikonxikonicstasker/Adapters/OrderAdapter.java
@@ -0,0 +1,165 @@
+package dragon.bakuman.iu.ikonxikonicstasker.Adapters;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import dragon.bakuman.iu.ikonxikonicstasker.Activities.DriverMainActivity;
+import dragon.bakuman.iu.ikonxikonicstasker.Fragments.DeliveryFragment;
+import dragon.bakuman.iu.ikonxikonicstasker.Objects.Order;
+import dragon.bakuman.iu.ikonxikonicstasker.R;
+
+public class OrderAdapter extends BaseAdapter {
+
+    private Activity activity;
+    private ArrayList<Order> orderList;
+
+    public OrderAdapter(Activity activity, ArrayList<Order> orderList) {
+        this.activity = activity;
+        this.orderList = orderList;
+    }
+
+    @Override
+    public int getCount() {
+        return orderList.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return orderList.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        if (view == null) {
+            view = LayoutInflater.from(activity).inflate(R.layout.list_item_order, null);
+        }
+
+        final Order order = orderList.get(i);
+
+        TextView restaurantName = (TextView) view.findViewById(R.id.restaurant_name);
+        TextView customerName = (TextView) view.findViewById(R.id.customer_name);
+        TextView customerAddress = (TextView) view.findViewById(R.id.customer_address);
+        ImageView customerImage = (ImageView) view.findViewById(R.id.customer_image);
+
+        restaurantName.setText(order.getRestaurantName());
+        customerName.setText(order.getCustomerName());
+        customerAddress.setText(order.getCustomerAddress());
+        Picasso.with(activity.getApplicationContext()).load(order.getCustomerImage()).fit().into(customerImage);
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Show an alert
+                AlertDialog.Builder builder = new AlertDialog.Builder((activity));
+                builder.setTitle("Pick this order?");
+                builder.setMessage("Would you like to take this order?");
+                builder.setPositiveButton("Cancel", null);
+                builder.setNegativeButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(activity.getApplicationContext(), "ORDER PICKED", Toast.LENGTH_SHORT).show();
+
+                        pickOrder(order.getId());
+                    }
+                });
+
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+        });
+
+        return view;
+    }
+
+    private void pickOrder(final String orderId) {
+        String url = activity.getString(R.string.API_URL) + "/driver/order/pick/";
+
+        StringRequest postRequest = new StringRequest
+                (Request.Method.POST, url, new Response.Listener<String>() {
+
+                    @Override
+                    public void onResponse(String response) {
+                        // Execute code
+                        Log.d("ORDER PICKED", response.toString());
+
+                        try {
+                            JSONObject jsonObj = new JSONObject(response);
+                            if (jsonObj.getString("status").equals("success")) {
+                                FragmentTransaction transaction = ((DriverMainActivity) activity).getSupportFragmentManager().beginTransaction();
+                                transaction.replace(R.id.content_frame, new DeliveryFragment()).commit();
+                            } else {
+                                Toast.makeText(activity, jsonObj.getString("error"), Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+                        Toast.makeText(activity, error.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                }) {
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                final SharedPreferences sharedPref = activity.getSharedPreferences("MY_KEY", Context.MODE_PRIVATE);
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("access_token", sharedPref.getString("token", ""));
+                params.put("order_id", orderId);
+
+                return params;
+            }
+        };
+
+        postRequest.setRetryPolicy(
+                new DefaultRetryPolicy(
+                        0,
+                        DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                        DefaultRetryPolicy.DEFAULT_BACKOFF_MULT)
+        );
+
+        RequestQueue queue = Volley.newRequestQueue(activity);
+        queue.add(postRequest);
+    }
+}
--- app/src/main/java/dragon/bakuman/iu/ikonxikonicstasker/Fragments/OrderListFragment.java
+++ app/src/main/java/dragon/bakuman/iu/ikonxikonicstasker/Fragments/OrderListFragment.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,7 +13,25 @@ import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import dragon.bakuman.iu.ikonxikonicstasker.Activities.MealListActivity;
+import dragon.bakuman.iu.ikonxikonicstasker.Adapters.OrderAdapter;
+import dragon.bakuman.iu.ikonxikonicstasker.Objects.Order;
+import dragon.bakuman.iu.ikonxikonicstasker.Objects.Restaurant;
 import dragon.bakuman.iu.ikonxikonicstasker.R;
 
 
@@ -20,6 +39,9 @@ import dragon.bakuman.iu.ikonxikonicstasker.R;
  * A simple {@link Fragment} subclass.
  */
 public class OrderListFragment extends Fragment {
+
+    private OrderAdapter adapter;
+    private ArrayList<Order> orderList;
 
 
     public OrderListFragment() {
@@ -38,36 +60,68 @@ public class OrderListFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        ListView restaurantListView = getActivity().findViewById(R.id.order_list);
-        restaurantListView.setAdapter(new BaseAdapter() {
-            @Override
-            public int getCount() {
-                return 3;
-            }
 
-            @Override
-            public Object getItem(int position) {
-                return null;
-            }
+        orderList = new ArrayList<>();
+        adapter = new OrderAdapter(this.getActivity(), orderList);
 
-            @Override
-            public long getItemId(int position) {
-                return 0;
-            }
+        ListView orderListView = getActivity().findViewById(R.id.order_list);
 
-            @Override
-            public View getView(int position, View convertView, ViewGroup parent) {
-                return LayoutInflater.from(getActivity()).inflate(R.layout.list_item_order, null);
-            }
-        });
+        orderListView.setAdapter(adapter);
 
-        restaurantListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(getContext(), MealListActivity.class);
-                startActivity(intent);
-            }
-        });
+        getReadyOrders();
+    }
+
+    private void getReadyOrders() {
+
+
+        String url = getString(R.string.API_URL) + "/driver/orders/ready/";
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        Log.d("READY ORDER LIST", response.toString());
+
+                        try {
+
+                            JSONArray ordersJSONArray = response.getJSONArray("orders");
+                            for (int i = 0; i < ordersJSONArray.length(); i++) {
+
+                                JSONObject orderObject = ordersJSONArray.getJSONObject(i);
+
+                                Order order = new Order(
+                                        orderObject.getString("id"),
+                                        orderObject.getJSONObject("registration").getString("name"),
+                                        orderObject.getJSONObject("customer").getString("name"),
+                                        orderObject.getString("address"),
+                                        orderObject.getJSONObject("customer").getString("avatar")
+
+                                );
+
+                                orderList.add(order);
+
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        adapter.notifyDataSetChanged();
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
+        );
+
+        RequestQueue queue = Volley.newRequestQueue(getActivity());
+        queue.add(jsonObjectRequest);
 
     }
 
